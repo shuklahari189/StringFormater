0411f8930a73106aca89cc580458b90ffff891d8
hari <shuklahari189@gmail.com>
2024-05-15 14:42:53 +0530

    removed functionality from / to onlyUnderscore

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 
 app.get("/", function (req, res) {
+    res.send("welcome");
+});
+
+app.get("/onlyUnderscore", function (req, res) {
     let str = req.headers["str"];
     let arrWithNoSpace = str.replace(/\s+/g, "_");
     let arrWithNoDots = arrWithNoSpace.replace(/\./g, "");
